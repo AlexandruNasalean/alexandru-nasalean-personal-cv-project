7f30841a081d0fb651eec18d37961750191aa3a9
AlexandruNasalean <58949902+AlexandruNasalean@users.noreply.github.com>
2020-06-15 13:58:09 +0300

    Update ExperiencePage.js

--- src/Components/ExperiencePage.js
+++ src/Components/ExperiencePage.js
@@ -1,14 +1,29 @@
 import React, { Component } from "react";
+import Tabs from 'react-bootstrap/Tabs'
+import ListGroup from 'react-bootstrap/ListGroup'
+import TabContainer from 'react-bootstrap/TabContainer'
+import Tab from 'react-bootstrap/Tab'
+import TabContent from 'react-bootstrap/TabContent'
 
 export class ExperiencePage extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {  }
-    }
+ 
     render() { 
         return ( 
-            <div>
-                <h1>Hullo Experience</h1>
+            <div className="container">
+                <h1>Experience</h1>
+                <div>
+                <Tabs defaultActiveKey="home" transition={false} id="noanim-tab-example">
+  <Tab eventKey="home" title="Home">
+    <Sonnet />
+  </Tab>
+  <Tab eventKey="profile" title="Profile">
+    <Sonnet />
+  </Tab>
+  <Tab eventKey="contact" title="Contact" disabled>
+    <Sonnet />
+  </Tab>
+</Tabs>
+                </div>
             </div>
          );
     }
